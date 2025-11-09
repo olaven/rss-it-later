95def5bd984a1ff9c712fd349761b73d04fc9cab
olaven <olav@sundfoer.com>
2025-11-09 21:56:41 +0100

    use DATABASE_URL in code to accomodate fly.io's defaults

--- src/main/kotlin/org/olaven/rssitlater/database/entities/Database.kt
+++ src/main/kotlin/org/olaven/rssitlater/database/entities/Database.kt
@@ -8,10 +8,9 @@ import org.olaven.rssitlater.environment
 // TODO: make this more generic
 fun connectDatabase(): Database {
     return Database.connect(
-        url = "jdbc:postgresql://${environment["PGHOST"]}:${environment["PGPORT"]}/${environment["PGDATABASE"]}",
+        url = "jdbc:${environment["DATABASE_URL"]}",
+//        url = "jdbc:postgresql://${environment["PGHOST"]}:${environment["PGPORT"]}/${environment["PGDATABASE"]}",
         driver = "org.postgresql.Driver",
-        user = environment["PGUSER"],
-        password = environment["PGPASSWORD"]
     )
 }
 
